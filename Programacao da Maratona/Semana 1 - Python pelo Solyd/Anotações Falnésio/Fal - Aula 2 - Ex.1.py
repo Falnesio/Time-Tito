8cca41e6cacced6990185cdf4bb5748e9f7be104
Falnésio Borges <falnesio.ghander@economia.ufjf.br>
2018-12-17 21:11:56 -0200

    Create Fal - Aula 2 - Ex.1.py

---
+++ Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/Fal - Aula 2 - Ex.1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 17 21:07:25 2018
+
+Anotações - Aula 2 - Entrada, saída, variáveis e operações matemáticas - Python Básico Solyd.md
+
+Ex.1
+
+@author: Fal
+"""
+
