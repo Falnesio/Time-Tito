571dbb85e49ab3a9ab8a3af29307299bf5211310
Falnésio Borges <falnesio.ghander@economia.ufjf.br>
2018-12-19 00:03:39 -0200

    adicionar conteúdo

---
+++ Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/temp_sandbox.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 18 23:29:46 2018
+
+@author: Fal
+"""
+
+frase = "Oi, tudo bem?"
+lista_nomes = ["João", "Maria", "Diego", "Fal"]
+
+print(frase)
+print(frase[0])
+print(type(frase))
+print(type(lista_nomes))
+print(lista_nomes[2])
+print(frase[0:13:2])
+print(lista_nomes[-1:-4: -1])
+
