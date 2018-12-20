10c0fcade862c00835c62e5ad63ec65d527781e3
Falnésio Borges <falnesio.ghander@economia.ufjf.br>
2018-12-20 00:33:37 -0200

    Update temp_sandbox.py

--- Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/temp_sandbox.py
+++ Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/temp_sandbox.py
@@ -5,13 +5,51 @@ Created on Tue Dec 18 23:29:46 2018
 @author: Fal
 """
 
-i = 0
-while i < 10:
-    print("i ainda é menor que 10", i)
-    i += 1
-print("fin", i)
+# Lista ou list (número modificável de ítens ordenados)
+lista_nomes = ["Fernando", "Susana"]
 
-i += 10
+# Tupla ou tuple (mesmo número finito de dados ordenados e não modificáveis)
+tupla_nomes = ("Fernando", "Susana")
 
-print(i)
+# Dicionário ou dict (ítens divididos por vírgulas)
+dicionario_cadastro = {"nome" : "Falnésio", "idade" : 36}
+print(dicionario_cadastro.keys())
 
+# Conjunto ou set (não tem ítens repetidos e não é ordenado)
+conjunto = {"Fal", "Marcela"}
+
+
+x = "Maria"
+if x in conjunto:
+    print("found")
+else:
+    print("not found")
+    
+    
+x = []
+'''
+criar múltiplos dicionários
+'''
+names = []
+for i in range(30,35):
+    names.append(str(i))
+    print(type(i))
+for i in names:
+    print(type(i))
+#names=["lloyd", "alice", "tyler"]
+keys=["homework", "quizzes", "tests"]
+steven={ name.capitalize():{key:[] for key in range(5)} for name in names}
+print(steven)  
+
+
+'''
+for i in range(58):
+    x.append(dicionario_cadastro)
+j = 0
+for i in x:
+    i["indice"] = j
+    print(i)
+    j += 1
+    
+print(x)
+'''
