fda1e52896eb7ad240fc89ccd0e88e6dd7e8c88a
Falnésio Ghander Soares Borges <36005374+Falnesio@users.noreply.github.com>
2018-12-22 23:44:46 -0200

    Create dicks.py

---
+++ Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/dicks.py
@@ -0,0 +1,29 @@
+'''
+criar múltiplos dicionários
+'''
+def dicks (length, lista): 
+    names = []
+    for i in range(length):
+        names.append(str(i))
+        #print(type(i))
+    for i in names:
+        #print(type(i))
+    keys= lista
+    steven={ name.capitalize():{key:[] for key in keys} for name in names}
+    print(steven)  
+
+a = dicks(10, ["idade", "raça"])
+print(a)
+
+
+```
+criar múltiplos dicionários pré-definidos
+```
+def dicks (lista_nomes, lista_categorias): 
+    names = lista_nomes
+    keys= lista_categorias
+    matrix={ name.capitalize():{key:[] for key in keys} for name in names}
+    return matrix  
+
+relatório = dicks (["Bob", "Luana", "João", "Tany", "Matias"], ["Empregos", "Escolaridade", "Cor", "Idade", "Sexo"])
+print(relatório)
