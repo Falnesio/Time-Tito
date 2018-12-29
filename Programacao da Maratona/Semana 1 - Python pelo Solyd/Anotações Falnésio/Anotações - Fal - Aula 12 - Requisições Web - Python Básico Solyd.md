1fd069ad6b5d83e528bec4c292b6b90f6ad9a029
Fal <falnesio.ghander@economia.ufjf.br>
2018-12-29 20:43:30 -0200

    add conteudo
    Signed-off-by: Fal <falnesio.ghander@economia.ufjf.br>

--- Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/Anotações - Fal - Aula 12 - Requisições Web - Python Básico Solyd.md
+++ Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/Anotações - Fal - Aula 12 - Requisições Web - Python Básico Solyd.md
@@ -13,10 +13,22 @@ python3 -m pip install requests
 
 import requests
 
-requisição = none
+cabeçalho = {"User-agent": "Windows 12",
+             "Referer": "https://google.com",
+             "cf-ipcountry": "US"}
 
+meus_biscoitos = {"ultima-visita": "10-10-2020"} #cookies
+
+dados = {"username":"flafal",
+         "senha": "adasd2243"}
+
+
+#puxando de putsreq.com
 try:
-    requisição = requests.get(https://google.com)
+    requisição = requests.post("https://putsreq.com/OCShNUJtBaVxEnh2HflW",
+                               headers = cabeçalho,
+                               cookies = meus_biscoitos,
+                               data = dados) #("https://putsreq.com/OCShNUJtBaVxEnh2HflW/inspect") #("https://google.com")
 except Exception as e:
     print("Requisição de erro:",e)
 
--- Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/Fal - Aula 12.py
+++ Programacao da Maratona/Semana 1 - Python pelo Solyd/Anotações Falnésio/Fal - Aula 12.py
@@ -4,11 +4,27 @@ Created on Fri Dec 28 23:26:41 2018
 
 @author: Fal
 """
+from typing import Dict, Any
 
 import requests
+import bs4
 
+cabeçalho = {"User-agent": "Windows 12",
+             "Referer": "https://google.com",
+             "cf-ipcountry": "US"}
+
+meus_biscoitos = {"ultima-visita": "10-10-2020"} #cookies
+
+dados = {"username":"flafal",
+         "senha": "adasd2243"}
+
+
+#puxando de putsreq.com
 try:
-    requisição = requests.get("https://google.com")
+    requisição = requests.post("https://putsreq.com/OCShNUJtBaVxEnh2HflW",
+                               headers = cabeçalho,
+                               cookies = meus_biscoitos,
+                               data = dados) #("https://putsreq.com/OCShNUJtBaVxEnh2HflW/inspect") #("https://google.com")
 except Exception as e:
     print("Requisição de erro:",e)
 
