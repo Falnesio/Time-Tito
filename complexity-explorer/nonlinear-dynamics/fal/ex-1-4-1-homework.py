728110313bddd0141b7058fccece29d6d491f799
falnesio <falnesio.ghander@economia.ufjf.br>
2019-02-05 22:39:07 -0200

    Track 2 files into repository.
    - modified complexity-explorer/nonlinear-dynamics/fal/ex-1-4-1-homework.py
    - modified .idea/workspace.xml
    
    Auto commit by GitBook Editor

--- complexity-explorer/nonlinear-dynamics/fal/ex-1-4-1-homework.py
+++ complexity-explorer/nonlinear-dynamics/fal/ex-1-4-1-homework.py
@@ -3,7 +3,7 @@ from pylab import *
 r = float(input("Insert r value: "))
 x1 = 0.2
 x2 = 0.200001
-n = 200
+n = 500000
 i = 0
 x1_menos_x2 = []
 lista_i = []
@@ -15,7 +15,8 @@ while i < n:
     lista_i.append(i)
     x1_menos_x2.append(abs(x1 - x2))
 
-
+print("Média da diferença absoluta para tragetória dos valores iniciais: ",
+      sum(x1_menos_x2) / n)
 print("x1: ", x1)
 print("x2: ", x2)
 plt.plot(lista_i, x1_menos_x2, c="b", marker="o", linestyle='None')
