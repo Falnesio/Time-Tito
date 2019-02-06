7b1c487daf69202e5a5d681a9d94d71542f611e9
falnesio <falnesio.ghander@economia.ufjf.br>
2019-02-05 22:13:49 -0200

    Track 4 files into repository.
    - modified .idea/workspace.xml
    - untracked complexity-explorer/nonlinear-dynamics/fal/ex-1-4-1-homework.py
    - untracked complexity-explorer/nonlinear-dynamics/fal/ex-1-2-2-logistics-homework.py
    - removed complexity-explorer/nonlinear-dynamics/fal/ex-1-2-2-logistics.py
    
    Auto commit by GitBook Editor

---
+++ complexity-explorer/nonlinear-dynamics/fal/ex-1-4-1-homework.py
@@ -0,0 +1,22 @@
+from pylab import *
+
+r = float(input("Insert r value: "))
+x1 = 0.2
+x2 = 0.200001
+n = 200
+i = 0
+x1_menos_x2 = []
+lista_i = []
+
+while i < n:
+    x1 = r * x1 * (1 - x1)
+    x2 = r * x2 * (1 - x2)
+    i += 1
+    lista_i.append(i)
+    x1_menos_x2.append(abs(x1 - x2))
+
+
+print("x1: ", x1)
+print("x2: ", x2)
+plt.plot(lista_i, x1_menos_x2, c="b", marker="o", linestyle='None')
+plt.show()
