ecb6f4b0a642b010777df04d4a23576b0a03bbcd
falnesio <falnesio.ghander@economia.ufjf.br>
2019-02-05 22:26:22 -0200

    Track 2 files into repository.
    - modified complexity-explorer/nonlinear-dynamics/fal/ex-1-2-2-logistics-homework.py
    - modified .idea/workspace.xml
    
    Auto commit by GitBook Editor

--- complexity-explorer/nonlinear-dynamics/fal/ex-1-2-2-logistics-homework.py
+++ complexity-explorer/nonlinear-dynamics/fal/ex-1-2-2-logistics-homework.py
@@ -1,10 +1,18 @@
+from pylab import *
+
 r = float(input("Insert r value: "))
 x = float(input("Insert starting x value: "))
 n = float(input("Insert number of iterations: "))
 i = 0
+lista_i = []
+lista_y = []
 
 while i < n:
-	x = r*x*(1-x)
-	i += 1
+    x = r * x * (1 - x)
+    i += 1
+    lista_i.append(i)
+    lista_y.append(x)
 
 print(x)
+scatter(lista_i, lista_y, s=100, marker='o')
+show()
